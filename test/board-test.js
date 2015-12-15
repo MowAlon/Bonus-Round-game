f32bf1bbd60aeb642c69d3a7bf7eb9ca1cc6e289
Alon Waisman <mowalon@yahoo.com>
2015-12-15 03:40:09 -0700

    Add more sounds

--- test/board-test.js
+++ test/board-test.js
@@ -3,48 +3,48 @@ const assert = chai.assert;
 
 const Board = require('../lib/board')
 
-describe('Board', function () {
+describe('Board', function() {
 
   it("should instantiate a new board", function(){
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.isObject(board)
   })
 
   it("should have a copy of the canvas size specs", function(){
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.equal(board.size.width, 500)
     assert.equal(board.size.height, 800)
   })
 
   it("should have five lanes", function(){
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.equal(board.lanes.length, 5)
   })
 
   it("should start with an empty array of bullets", function(){
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.isArray(board.bullets)
     assert.deepEqual(board.bullets, [])
   })
 
   it("should have two players", function(){
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.equal(board.players.length, 2)
   })
 
   it("should assign the first player's firing direction as 'up'", function() {
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.equal(board.players[0].fireDirection, 'up')
   })
 
   it("should assign the second player's firing direction as 'down'", function() {
-    let board = new Board({width: 500, height: 800})
+    let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
     assert.equal(board.players[1].fireDirection, 'down')
   })
 
   describe ('addPlayer', function() {
     it("should add a new player to the board's players array", function() {
-      let board = new Board({width: 500, height: 800})
+      let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
       let player = board.addPlayer('up')
       assert.include(board.players, player)
     })
@@ -52,21 +52,13 @@ describe('Board', function () {
 
   describe ('addLane', function() {
     it("should add a new lane to the board's lanes array", function() {
-      let board = new Board({width: 500, height: 800})
+      let board = new Board({shotDelay: 0}, {cannon: 0}, {width: 500, height: 800})
       var laneCountBefore = board.lanes.length
       var laneNumber = 0
       let lane = board.addLane(board, laneNumber)
       var laneCountAfter = board.lanes.length
       assert.include(board.lanes, lane)
       assert.equal(laneCountAfter, laneCountBefore + 1)
-    })
-  })
-
-  describe ('addBullet', function() {
-    it("should add a new bullet to the board's bullets array", function() {
-      let board = new Board({width: 500, height: 800})
-      let bullet = board.addBullet(board.lanes[0], board.players[0])
-      assert.include(board.bullets, bullet)
     })
   })
 
--- test/bullet-test.js
+++ test/bullet-test.js
@@ -6,36 +6,36 @@ const Bullet = require('../lib/bullet')
 
 describe('Bullet', function () {
   beforeEach(function() {
-    this.board = new Board({width: 500, height: 800})
+    this.board = new Board({shotDelay: 0}, {bulletBlack: {height: 0}, cannon: {height: 0}}, {width: 500, height: 800})
     this.lane = this.board.lanes[0]
     this.player = this.board.players[0]
   })
 
   it("should have a reference to the board", function() {
-    let bullet = new Bullet(this.board, this.lane, this.player)
+    let bullet = new Bullet(this.board, {bulletBlack: {height: 0, width: 0}}, this.lane, this.player)
     assert.equal(bullet.board, this.board)
   })
 
   it("should have an X-coordinate defined by its lane", function() {
     this.lane.x = 50
-    let bullet = new Bullet(this.board, this.lane, this.player)
+    let bullet = new Bullet(this.board, {bulletBlack: {height: 0, width: 0}}, this.lane, this.player)
     assert.strictEqual(bullet.x, 50)
   })
 
   it("should have a Y-coordinate equal to the board's height if the player's firing direction is 'up'", function() {
     this.player.fireDirection = 'up'
-    let bullet = new Bullet(this.board, this.lane, this.player)
+    let bullet = new Bullet(this.board, {bulletBlack: {height: 0, width: 0}}, this.lane, this.player)
     assert.strictEqual(bullet.y, this.board.size.height)
   })
 
-  it("should have a Y-coordinate less than zero if the player's firing direction is 'down'", function() {
+  it("should have a Y-coordinate equal to the cannon's height minus the bullet's height if the player's firing direction is 'down'", function() {
     this.player.fireDirection = 'down'
-    let bullet = new Bullet(this.board, this.lane, this.player)
-    assert(bullet.y < 0)
+    let bullet = new Bullet(this.board, {bulletBlack: {height: 1, width: 0}}, this.lane, this.player)
+    assert(bullet.y = this.board.graphics.cannon.height - this.board.graphics.bulletBlack.height)
   })
 
   it("should be included in the board's array of bullets", function() {
-    let bullet = new Bullet(this.board, this.lane, this.player)
+    let bullet = new Bullet(this.board, {bulletBlack: {height: 0, width: 0}}, this.lane, this.player)
     assert.include(this.board.bullets, bullet)
   })
 
